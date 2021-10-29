ceccea4cc5070d2c1a6f00bc337db54293b575fd
Sean Lau <asean528@gmail.com>
2021-10-29 16:49:33 +0800

    first commit

---
+++ music-list.js
@@ -0,0 +1,110 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name:"你不知道",
+      artist:"柯西瓜_刘羽晟",
+      img:"你不知道",
+      src:"你不知道",
+    },
+    {
+      name:"命里缺你",
+      artist:"刘羽晟",
+      img:"命里缺你",
+      src:"命里缺你",
+    },
+    {
+      name:"凯度来客",
+      artist:"赵百川_刘羽晟",
+      img:"凯度来客",
+      src:"凯度来客"
+    },
+    {
+      name:"采茶",
+      artist:"刘羽晟_洛天依",
+      img:"采茶",
+      src:"采茶"
+    },
+    {
+      name:"识破",
+      artist:"赵百川_刘羽晟",
+      img:"识破",
+      src:"识破"
+    },
+    {
+      name:"退路",
+      artist:"刘羽晟_刘瑞瑞",
+      img:"退路",
+      src:"退路"
+    },
+    {
+      name:"极夜",
+      artist:"赵百川_刘羽晟",
+      img:"极夜",
+      src:"极夜"
+    },
+    {
+      name:"超级无敌喜欢你",
+      artist:"刘羽晟",
+      img:"超级无敌喜欢你",
+      src:"超级无敌喜欢你"
+    },
+    {
+      name:"繁星Demo",
+      artist:"刘羽晟_unknown",
+      img:"繁星Demo",
+      src:"繁星Demo"
+    },
+    {
+      name:"给陌生的你听",
+      artist:"刘羽晟_张思源",
+      img:"给陌生的你听",
+      src:"给陌生的你听"
+    },
+    {
+      name:"instrumental-1",
+      artist:"刘羽晟",
+      img:"instrumental1",
+      src:"instrumental1"
+    },
+    {
+      name:"instrumental-2",
+      artist:"刘羽晟",
+      img:"instrumental2",
+      src:"instrumental2"
+    },
+    {
+      name:"画Cover",
+      artist:"刘羽晟",
+      img:"画Cover",
+      src:"画Cover"
+    },
+    {
+      name:"still shine",
+      artist:"刘羽晟",
+      img:"stillshine",
+      src:"stillshine"
+    },
+    {
+      name:"boom clap remix",
+      artist:"刘羽晟",
+      img:"boomclapremix",
+      src:"boomclapremix",
+    },
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
